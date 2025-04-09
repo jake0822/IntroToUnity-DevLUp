fae6dd31d34486afe0d3681695ebfbaa5cd35365
jake0822 <jakeyounan16@gmail.com>
2025-04-09 14:25:06 -0400

    adds scoring to game

---
+++ Assets/ScoreCalculator.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ScoreCalculator : MonoBehaviour
+{
+    private float timeAlive;
+    private bool PlayerScoring;
+
+    public TextMeshPro[] Texts;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerScoring = true;
+        timeAlive = 0;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PlayerScoring = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (PlayerScoring == true)
+        {
+            timeAlive += Time.deltaTime;
+            foreach (var text in Texts)
+            {
+                text.text = MathF.Round(timeAlive).ToString();
+            }
+        }
+    }
+}
